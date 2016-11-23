1ecd039db81a277f566abbfe9249b2b87f1d777c
Daoping Wang <wang.daoping@outlook.sg>
2016-11-23 15:08:04 +0100

    update DepthFirstSearcher, uses stack for cycle detection, terrible performance

--- src/DataBackend/DepthFirstSearcher.java
+++ src/DataBackend/DepthFirstSearcher.java
@@ -5,10 +5,25 @@ import java.util.Stack;
 /**
  * Created by Wang.Daoping on 23.11.2016.
  */
+
+/**
+ * is implemented after finding out BreadthFirstSearcher's fatal problems with cycle detection and
+ * calculation inaccuracy. DepthFirstSearcher is basically based on recursive DFS (hence it's name).
+ * It traverses through the graph prioritising the latest discovered nodes and avoids running in circle
+ * by comparing the next node with the stack which holds all the lately visited nodes. After all parents
+ * of a node have been visited, it will be removed from the stack.
+ *
+ * The performance of this algorithm is horrible. It needs around 45 minutes for the computation of all the paths.
+ */
 public class DepthFirstSearcher {
     public static Stack<KeywordVertex> stack = new Stack<>();
     public static int discoveredLength;
 
+    /**
+     * computes all path lengths between the given node and it's parents by recursively calling dfs() method.
+     * After the computation, it updates pathLength[] of the given vertex.
+     * @param vertex which is given.
+     */
     public static void performDFS(KeywordVertex vertex){
         int maxLength = 0;
         DepthFirstSearcher.stack.push(vertex);
@@ -18,10 +33,11 @@ public class DepthFirstSearcher {
             CSVParser.keywordArray[i].tempLayer = 0;
         }
 
-        for(int i = 0; i < vertex.parentNum; i++){
+        for(int i = 0; i < vertex.parentNum && !vertex.alreadyCalculated; i++){
             for(int j = 0; j < CSVParser.keywordEntries; j++){
                 if(vertex.parent[i].equals(CSVParser.keywordArray[j].name) && !vertex.parent[i].equals("Mercateo")){
-                    DepthFirstSearcher.dfs(CSVParser.keywordArray[j]);
+                    DepthFirstSearcher.stack.push(CSVParser.keywordArray[j]);
+                    DepthFirstSearcher.dfs(DepthFirstSearcher.stack);
                     vertex.pathLength[i] = DepthFirstSearcher.discoveredLength + 1;
                     if(maxLength < DepthFirstSearcher.discoveredLength + 1){
                         maxLength = DepthFirstSearcher.discoveredLength + 1;
@@ -33,13 +49,20 @@ public class DepthFirstSearcher {
             }
         }
 
-        if(vertex.noLayerSet()){
+        if(vertex.noLayerSet() && !vertex.alreadyCalculated){
             vertex.setLayer(maxLength);
         }
     }
 
-    private static void dfs(KeywordVertex v){
-        for(int i = 0; i < v.parentNum; i++){
+    /**
+     * peeks the first vertex from the stack, pushes one of it's parents if it doesn't exist on the stack, then
+     * calls itself recursively. When an end is reached, it goes one stage back, checks if there is any parent left,
+     * if not, remove that node from the stack.
+     * @param stack
+     */
+    private static void dfs(Stack<KeywordVertex> stack){
+        KeywordVertex v = stack.peek();
+        for(int i = 0; i < v.parentNum && !v.alreadyCalculated; i++){
             for(int j = 0; j < CSVParser.keywordEntries; j++){
                 if(v.parent[i].equals(CSVParser.keywordArray[j].name) && !v.parent[i].equals("Mercateo")){
                     if(!isOnStack(CSVParser.keywordArray[j], DepthFirstSearcher.stack)){
@@ -48,7 +71,7 @@ public class DepthFirstSearcher {
                         if(DepthFirstSearcher.discoveredLength < CSVParser.keywordArray[j].tempLayer){
                             DepthFirstSearcher.discoveredLength = CSVParser.keywordArray[j].tempLayer;
                         }
-                        DepthFirstSearcher.dfs(CSVParser.keywordArray[j]);
+                        DepthFirstSearcher.dfs(DepthFirstSearcher.stack);
                     }
                     break;
                 } else if(v.parent[i].equals(CSVParser.keywordArray[j].name) && v.parent[i].equals("Mercateo")){
@@ -56,7 +79,7 @@ public class DepthFirstSearcher {
                 }
             }
         }
-
+        v = DepthFirstSearcher.stack.pop();
     }
 
     private static boolean isOnStack(KeywordVertex vertex, Stack stack){
